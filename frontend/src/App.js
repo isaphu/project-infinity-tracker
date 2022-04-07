cfa42850938e7f115799bd999105d20eddaf8ff2
Isa Phuyuthanon <uronohan@gmail.com>
2022-04-07 23:23:33 +0700

    adding path and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//adding pages
+import Home from "./pages/public/home";
 
-function App() {
+//adding css
+import "./css/home.css"
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        //adding more routes here
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
