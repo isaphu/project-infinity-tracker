181dc1be9b03d328d768c1ec7225790eafe4956b
Isa Phuyuthanon <uronohan@gmail.com>
2022-04-11 13:29:49 +0700

    small change on path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 //adding pages
 import Home from "./pages/public/home";
-import Login from "./pages/public/login";
+import InternalAccess from "./pages/public/access-form";
 import Activity from "./pages/private/activities-form";
 import Dashboard from "./pages/private/dashboard";
 import Profile from "./pages/private/profile";
@@ -9,7 +9,7 @@ import Profile from "./pages/private/profile";
 //adding css respective to the pages
 import "./css/home.css"
 import "./css/main.css"
-import "./css/login.css"
+import "./css/internal-access.css"
 import "./css/activities-form.css"
 import "./css/dashboard.css"
 import "./css/profile.css"
@@ -22,8 +22,8 @@ export default function App() {
       <Routes>
       //adding routes to pages here...
         <Route path="/" exact element={<Home />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/activitiesform" exact element={<Activity />} />
+        <Route path="/internal-access" exact element={<InternalAccess />} />
+        <Route path="/create-activities" exact element={<Activity />} />
         <Route path="/dashboard" exact element={<Dashboard />} />
         <Route path="/profile" exact element={<Profile />} />
       </Routes>
