71a74c3013c6633949cf4591441a90e723437bf5
Isa Phuyuthanon <uronohan@gmail.com>
2022-04-13 22:35:13 +0700

    adding CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cors = require("cors")
 const connectDB = require("./config/database.js");
 const Activity = require("./models/Activity.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 //connect databse
 
@@ -43,3 +44,57 @@ server.post("/api/v1/createActivities", async function (request, response) {
   });
 });
 
+// (R) READ activities
+
+server.get("/api/v1/createActivities", async function (request, response) {
+  const activities = await Activity.find();
+
+  if (!activities) {
+    response.status(400).json({
+      success: false,
+      message: "No activity found",
+    });
+  }
+  response.status(200).json({
+    success: true,
+    count: activities.length,
+    data: activities,
+  });
+});
+
+// (U) Update activites
+
+server.put("/api/v1/createActivities/:id", async function (request, response) {
+  //update activity by ID
+  const updateActivity = await Activity.findByIdAndUpdate(
+    request.params.id,
+    request.body
+  );
+
+  if (!updateActivity) {
+    response.status(400).json({
+      success: true,
+      message: `No activity found with the id of ${request.params.id}`,
+    });
+  }
+
+  response.status(200).json({
+    success: true,
+    data: updateActivity,
+  });
+});
+
+// (D) Delete activity
+
+server.delete("/api/v1/createActivities/:id", async function (request, response) {
+  //deleteOne => delete by id
+    const result = await Activity.deleteOne({
+      _id: request.params.id,
+    });
+
+    response.status(200).json({
+      success: true,
+      data: result,
+    });
+  }
+);
