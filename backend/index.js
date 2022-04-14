e81e45f9748a763d3d39ea7d93268f00c888e692
Isa Phuyuthanon <uronohan@gmail.com>
2022-04-14 12:43:25 +0700

    new routes

--- backend/index.js
+++ backend/index.js
@@ -1,100 +1,30 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const app = express();
+const cors = require("cors");
 const connectDB = require("./config/database.js");
-const Activity = require("./models/Activity.js");
+const activityRoutes = require("./routes/activity.js");
+const userRoutes = require("./routes/users.js");
+const authRoutes = require("./routes/auth.js");
 
-const server = express();
-
-server.use(express.json());
-server.use(cors());
-
-//connect databse
-
+//connect database
 connectDB();
 
-server.listen(5000, function () {
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("api/v1/users", userRoutes);
+app.use("api/v1/auth", authRoutes);
+app.post("/api/v1/createActivities", activityRoutes);
+app.get("/api/v1/createActivities", activityRoutes);
+app.put("/api/v1/createActivities", activityRoutes);
+app.delete("/api/v1/createActivities", activityRoutes);
+
+
+app.listen(5000, function () {
   console.log("server is running on PORT: 5000");
-});
+});  
 
-server.get("/", function (request, response) {
-  //   response.send("Welcome to infinity-tracker-backend");
-  response.status(200).json({
-    success: true,
-    data: "Welcome to infinity-tracker-backend",
-  });
-});
 
-// CRUD routes
-
-// (C) CREATE activity
-
-server.post("/api/v1/createActivities", async function (request, response) {
-  const activity = await Activity.create(request.body);
-  //check if there is no activity created will response with 400 http code
-  if (!activity) {
-    response.status(400).json({
-      success: false,
-      message: "Activity not created",
-    });
-  }
-  //else will response with http code 200
-  response.status(200).json({
-    success: true,
-    data: activity,
-  });
-});
-
-// (R) READ activities
-
-server.get("/api/v1/createActivities", async function (request, response) {
-  const activities = await Activity.find();
-
-  if (!activities) {
-    response.status(400).json({
-      success: false,
-      message: "No activity found",
-    });
-  }
-  response.status(200).json({
-    success: true,
-    count: activities.length,
-    data: activities,
-  });
-});
-
-// (U) Update activites
-
-server.put("/api/v1/createActivities/:id", async function (request, response) {
-  //update activity by ID
-  const updateActivity = await Activity.findByIdAndUpdate(
-    request.params.id,
-    request.body
-  );
-
-  if (!updateActivity) {
-    response.status(400).json({
-      success: true,
-      message: `No activity found with the id of ${request.params.id}`,
-    });
-  }
-
-  response.status(200).json({
-    success: true,
-    data: updateActivity,
-  });
-});
-
-// (D) Delete activity
-
-server.delete("/api/v1/createActivities/:id", async function (request, response) {
-  //deleteOne => delete by id
-    const result = await Activity.deleteOne({
-      _id: request.params.id,
-    });
-
-    response.status(200).json({
-      success: true,
-      data: result,
-    });
-  }
-);
